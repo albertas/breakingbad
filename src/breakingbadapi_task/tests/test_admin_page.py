30691e58d6ad0f7e360a55f3ac17032c1fb3754d
niekas <albertasgim@gmail.com>
2020-11-07 12:42:54 +0000

    Add swagger page
    Remove admin page, since it won't be needed.

--- src/breakingbadapi_task/tests/test_admin_page.py
+++ src/breakingbadapi_task/tests/test_admin_page.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 
 
-class AdminPageTests(TestCase):
-    def test_admin_page(self):
-        resp = self.client.get(reverse("admin:index"))
-        self.assertEqual(resp.status_code, 302)
+class SwaggerPageTests(TestCase):
+    def test_swagger_ui_page(self):
+        resp = self.client.get(reverse("swagger"))
+        self.assertEqual(resp.status_code, 200)
