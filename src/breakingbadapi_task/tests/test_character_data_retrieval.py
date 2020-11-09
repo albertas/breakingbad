ae17be5bd7afa0beaf486266da7db1c164bc4b6d
niekas <albertasgim@gmail.com>
2020-11-09 05:05:59 +0000

    Add test for loading data from external API

--- src/breakingbadapi_task/tests/test_character_data_retrieval.py
+++ src/breakingbadapi_task/tests/test_character_data_retrieval.py
@@ -1,7 +1,11 @@
 import json
-from unittest import TestCase, mock
+from unittest import mock
 
-from breakingbadapi_task.utils import get_character_data_from_external_api
+from django.test import TestCase
+
+from breakingbadapi_task.models import Character
+from breakingbadapi_task.utils import (get_character_data_from_external_api,
+                                       load_character_data_from_external_api)
 
 
 class CharacterDataRetrievalTests(TestCase):
@@ -17,3 +21,18 @@ class CharacterDataRetrievalTests(TestCase):
         self.assertEqual(characters[0]["name"], "Walter White")
         self.assertEqual(characters[0]["status"], "Presumed dead")
         self.assertEqual(characters[0]["birthday"], "09-07-1958")
+
+    @mock.patch("breakingbadapi_task.utils.requests.get")
+    def test_character_data_loading_from_external_api(self, get_mock):
+        sample_filename = "src/breakingbadapi_task/fixtures/character_sample.json"
+        m = mock.MagicMock()
+        m.json.return_value = json.load(open(sample_filename))
+        get_mock.return_value = m
+
+        load_character_data_from_external_api()
+        self.assertEqual(Character.objects.count(), 4)
+
+        first_character = Character.objects.first()
+        self.assertEqual(first_character.name, "Walter White")
+        self.assertEqual(first_character.status, "Presumed dead")
+        self.assertEqual(str(first_character.birthday), "1958-09-07")
