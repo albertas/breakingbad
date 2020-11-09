678664cdd7dcbeeaa0d88835d1c236829557b347
niekas <albertasgim@gmail.com>
2020-11-09 03:44:44 +0000

    Move fixtures dir to app root from tests/fixtures
    since its default place for fixtures.

--- src/breakingbadapi_task/tests/test_character_data_retrieval.py
+++ src/breakingbadapi_task/tests/test_character_data_retrieval.py
@@ -7,7 +7,7 @@ from breakingbadapi_task.utils import get_character_data_from_external_api
 class CharacterDataRetrievalTests(TestCase):
     @mock.patch("breakingbadapi_task.utils.requests.get")
     def test_character_data_retrieval_from_external_api(self, get_mock):
-        sample_filename = "src/breakingbadapi_task/tests/fixtures/character_sample.json"
+        sample_filename = "src/breakingbadapi_task/fixtures/character_sample.json"
         m = mock.MagicMock()
         m.json.return_value = json.load(open(sample_filename))
         get_mock.return_value = m
