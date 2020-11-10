f66359bac20960dd699dd9cf4e31b07599d982cb
niekas <albertasgim@gmail.com>
2020-11-10 07:05:25 +0000

    Use PostGIS database for tests also
    since spacial operations have to be tested also.

--- src/breakingbadapi_task/settings/test.py
+++ src/breakingbadapi_task/settings/test.py
@@ -1,3 +1,12 @@
 from .base import *
 
-DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3"}}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": "test",
+        "USER": "postgres",
+        "PASSWORD": "test",
+        "HOST": "db",
+        "PORT": 5432,
+    }
+}
